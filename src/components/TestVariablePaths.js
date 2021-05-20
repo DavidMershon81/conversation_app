efc141217d6c5ead2d9c9be3bf1abd10de761af8
= <dmershon@universalhappymaker.com>
2021-05-20 10:42:19 -0700

    a little cleanup from the last commit

--- src/components/TestVariablePaths.js
+++ src/components/TestVariablePaths.js
@@ -3,13 +3,13 @@ import { useLocation } from 'react-router-dom';
 const TestVariablePaths = ({basePath}) => {
     const location = useLocation();
     const testPath = location.pathname.replace(basePath, '');
-    console.log('testPath:' + testPath);
+
     return (
         <section>
             <h3>Testing Variable Paths</h3>
             <p>subpath: {testPath} </p>
         </section>
-    )
+    );
 }
 
 export default TestVariablePaths;
