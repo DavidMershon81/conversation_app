707bb205902fb5f99bcc726dd6ae02ac2748c0b4
= <dmershon@universalhappymaker.com>
2021-06-05 14:10:53 -0700

    login endpoint generates and returns a jwt token. added pyjwt to requirements.txt

--- api/app/login_endpoints.py
+++ api/app/login_endpoints.py
@@ -1,17 +1,28 @@
-from flask import request, jsonify
+from flask import request, make_response, jsonify
+import jwt
+import datetime
+from dotenv import dotenv_values
 from app import app
 from app import database as db
 from werkzeug.security import check_password_hash
 
+config = dotenv_values("app_env")
+secret_key = config['JWT_SECRET_KEY']
+
 @app.route('/api/login', methods=['POST'])
 def login():
-    json = request.json
-    login_user = db.get_user_by_email(json['email'])
+    auth = request.authorization
+    login_user = db.get_user_by_email(auth.username)
+    
     if login_user:
-        password_matches = check_password_hash(login_user.password, json['password'])
+        password_matches = check_password_hash(login_user.password, auth.password)
         if password_matches:
-            return f"login attempt - email: {json['email']} - login attempt successful"
+            return generate_token(auth.username)
         else:
-            return f"login attempt - email: {json['email']} - found user, but password doesn't match"
+            return make_response(f"invalid password", 401, {'WWW-Authenticate' : 'Basic realm="Login Required"'})
     else:
-        return f"login attempt - email: {json['email']} - can't find user"
+        return make_response(f"invalid user", 401, {'WWW-Authenticate' : 'Basic realm="Login Required"'})
+
+def generate_token(email):
+    token = jwt.encode({ 'email' : email, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=30) }, secret_key)
+    return jsonify({'token' : token })
