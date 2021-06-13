0fdc18b6f1285435ba91d06a234f49e5a98cd9da
= <dmershon@universalhappymaker.com>
2021-06-13 14:30:57 -0700

    removed post method from members endpoint because listserv member adding is not yet ready

--- api/app/member_endpoints.py
+++ api/app/member_endpoints.py
@@ -2,15 +2,14 @@ from flask import request, jsonify
 from app import app, tokens
 from app import database as db
 
-@app.route('/api/members', methods=['GET', 'POST'])
+@app.route('/api/members', methods=['GET'])
 @tokens.token_required
 def members(current_user):
-    print(f"hit members endpoint method:{request.method}")
+    #this will have a post menthod eventually when listserv groups are fleshed out
+    #but right now the only time new members are added is when a petition group is created
     if request.method == 'GET':
         petition_group_id = request.args['petition_group_id']
         return jsonify([ member_to_dict(member) for member in db.get_members(petition_group_id)])
-    elif request.method == 'POST':
-        return db.add_members_to_petition(request.json, request.json['petition_group_id'])
 
 def member_to_dict(member):
     return { 'id' : member.id, 'email' : member.email, 'petition_group_id' : member.petition_group_id }
