cb7ac07971ce7ba8ed414565deea2fe524bffdf6
= <dmershon@universalhappymaker.com>
2021-06-13 19:40:44 -0700

    petition group views can hide members list

--- src/components/PetitionGroupView.js
+++ src/components/PetitionGroupView.js
@@ -1,55 +1,63 @@
 import { useLocation, Link, Redirect } from 'react-router-dom';
 import useFetchDataAuth from  '../hooks/useFetchDataAuth'
-import { LoadingBox } from './MiscControls';
-import { useRef, useContext } from 'react';
+import { LoadingBox, SimpleButton } from './MiscControls';
+import { useRef, useContext, useState } from 'react';
 import { AppContext } from '../contexts/AppContext';
 
 const PetitionGroupPetitionsSection = ({ petitionGroupId }) => {
-    const getRequestParams = useRef({ petition_group_id : petitionGroupId });
-    const { data:petitions, loading, error, errorMessage } = useFetchDataAuth({ 
-        url:'/api/petitions', 
-        getRequestParams:getRequestParams.current
-    });
+  const getRequestParams = useRef({ petition_group_id : petitionGroupId });
+  const { data:petitions, loading, error, errorMessage } = useFetchDataAuth({ 
+      url:'/api/petitions', 
+      getRequestParams:getRequestParams.current
+  });
     
-    return (
-      <>
-      <br/><strong>Petitions</strong>
-      <LoadingBox loading={loading} error={error} errorMessage={errorMessage} />
-      <ul className='petitions_list_group'>
-      { (petitions && petitions.length > 0) ? petitions.map((petition, index) => 
-        <li className='petitions_list_item' key={index}>
-            <Link className='petitions_list_btn' to={`/petitions/${petition['id']}`}>View</Link>
-            <span>Subject: {petition['subject']}</span>
-        </li>) : 
-        <p>This group has no petition yet.</p>
-      }
-      </ul>
-        {petitions && <div className='add_petition_group_btn'>
-          <Link className='add_petition_group_text' to={`/add_petition/${petitionGroupId}`}>Add New Petition</Link>
-        </div>}
-      </>
-    );
+  return (
+    <section className='petition_group_section'>
+    <strong>Petitions</strong>
+    <LoadingBox loading={loading} error={error} errorMessage={errorMessage} />
+    <ul className='petitions_list_group'>
+    { (petitions && petitions.length > 0) ? petitions.map((petition, index) => 
+      <li className='petitions_list_item' key={index}>
+          <Link className='petitions_list_btn' to={`/petitions/${petition['id']}`}>View</Link>
+          <span>Subject: {petition['subject']}</span>
+      </li>) : 
+      <p>This group has no petition yet.</p>
+    }
+    </ul>
+      {petitions && <div className='add_petition_group_btn'>
+        <Link className='add_petition_btn_text' to={`/add_petition/${petitionGroupId}`}>Add New Petition</Link>
+      </div>}
+    </section>
+  );
 };
 
-const PetitionGroupMembersList = ({ petitionGroupId }) => {
-    const getRequestParams = useRef({ petition_group_id : petitionGroupId });
-    const { data:members, loading, error, errorMessage } = useFetchDataAuth({ 
-        url:'/api/members', 
-        getRequestParams:getRequestParams.current
-    });
-    
-    return (
-      <>
-      <br/><strong>Members</strong>
+const PetitionGroupMembersSection = ({ petitionGroupId }) => {
+  const getRequestParams = useRef({ petition_group_id : petitionGroupId });
+  const { data:members, loading, error, errorMessage } = useFetchDataAuth({ 
+      url:'/api/members', 
+      getRequestParams:getRequestParams.current
+  });
+
+  const [showMembers, setShowMembers] = useState(false);
+  const showMembersBtnText = showMembers ? 'Hide' : 'Show';
+
+  const onToggleMembers = (id, text) => {
+    setShowMembers(!showMembers);
+  };
+
+  return (
+    <section className='petition_group_section'>
+      <strong>Members</strong>
+      <SimpleButton className='full_width_btn' onBtnClick={onToggleMembers} text={showMembersBtnText}/>
       <LoadingBox loading={loading} error={error} errorMessage={errorMessage} />
-      <ul className='users_list_group'>
+      {showMembers && <ul className='users_list_group'>
       { (members && members.length > 0) ? members.map((member) => 
         <li className='users_list_item' key={member['email']}>{member['email']}</li>) : 
         <p>This group has no members yet.</p>
       }
-      </ul>
-      </>
-    );
+      </ul>}
+    </section>
+  );
 };
   
 const PetitionGroupSummary = ({ petitionGroup }) => {
@@ -71,12 +79,12 @@ const PetitionGroupView = ({ basePath }) => {
     return <Redirect to='/login' />
   }
   return (
-      <section>
+      <section className='petition_group_view'>
           <LoadingBox loading={loading} error={error} errorMessage={errorMessage} />
           { petitionGroup && (<>
           <h2>Petition Group: {petitionGroup.group_name}</h2>
           <PetitionGroupSummary petitionGroup={petitionGroup} />
-          <PetitionGroupMembersList petitionGroupId={petitionGroupId} />
+          <PetitionGroupMembersSection petitionGroupId={petitionGroupId} />
           <PetitionGroupPetitionsSection petitionGroupId={petitionGroup['id']} /></>) }
       </section>
   );
