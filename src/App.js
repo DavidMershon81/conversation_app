0f715c181740788296287e66c07a219fb68e2395
= <dmershon@universalhappymaker.com>
2021-06-13 18:59:43 -0700

    slight reformatting

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import DebugUsersView from './components/DebugUsersView';
 import PetitionGroupView from './components/PetitionGroupView';
 import AddPetitionGroupView from './components/AddPetitionGroupView';
@@ -7,7 +7,7 @@ import AddPetitionView from './components/AddPetitionView';
 import PetitionView from './components/PetitionView';
 import LoginView from './components/LoginView';
 import HomeView from './components/HomeView';
-import { SimpleButton } from './components/MiscControls';
+import TopNavSection from './components/TopNavSection';
 import { AppContext } from './contexts/AppContext';
 import { useState } from 'react';
 
@@ -15,23 +15,11 @@ const App = () => {
   const [authToken, setAuthToken] = useState("");
   const [loggedInUser, setLoggedInUser] = useState("");
 
-  const onLogout = (id, text) => {
-    setAuthToken("");
-    setLoggedInUser("");
-  };
-
   return (
       <div className="App">
           <AppContext.Provider value={{ authToken, setAuthToken, loggedInUser, setLoggedInUser }}>
-            <Router>      
-              <h1 className='top_heading_text'>Petition App Prototype</h1>
-              <nav className='top_nav'>
-                <Link className='nav_link' to='/debug/users'>Users(debug)</Link>
-                { authToken && <Link className='nav_link' to='/'>Home</Link> }
-                { !authToken && <Link className='nav_link' to='/login'>Login</Link> }
-                { authToken && <SimpleButton className='nav_link' onBtnClick={onLogout} text='Logout'/> }
-              </nav>
-              <p className='logged_in_user_text'>{loggedInUser}</p>
+            <Router>
+              <TopNavSection />
               <Switch>
                 <Route exact path='/' component={HomeView} />
                 <Route exact path='/login' component={LoginView} />
---
+++ src/components/TopNavSection.js
@@ -0,0 +1,29 @@
+
+import { SimpleButton } from './MiscControls';
+import { Link } from 'react-router-dom';
+import { useContext } from 'react'
+import { AppContext } from '../contexts/AppContext';
+
+const TopNavSection = () => {
+    const { loggedInUser, setAuthToken, setLoggedInUser } = useContext(AppContext);
+
+    const onLogout = (id, text) => {
+        setAuthToken("");
+        setLoggedInUser("");
+    };
+
+    return (
+        <section>
+            <h1 className='top_heading_text'>Petition App Prototype</h1>
+            <nav className='top_nav'>
+                <Link className='nav_link' to='/debug/users'>Users(debug)</Link>
+                { loggedInUser && <Link className='nav_link' to='/'>Home</Link> }
+                { !loggedInUser && <Link className='nav_link' to='/login'>Login</Link> }
+                { loggedInUser && <SimpleButton className='nav_link' onBtnClick={onLogout} text='Logout'/> }
+            </nav>
+            <p className='logged_in_user_text'>{loggedInUser}</p>
+        </section>   
+    );
+}
+
+export default TopNavSection
