4d59e519472575bee1f6f1b746b0a9831d029ada
= <dmershon@universalhappymaker.com>
2021-07-17 19:13:51 -0700

    added validation to register user form to make sure the password must match the confirm password field

--- src/components/FormControls.js
+++ src/components/FormControls.js
@@ -1,16 +1,25 @@
 import { useContext } from 'react';
 import { FormContext } from '../contexts/FormContext';
 
-const FormErrorMessage = ({ varName, visibleName }) => {
+const FormErrorMessage = ({ varName, visibleName, validateErrorMessage }) => {
     const { errors } = useContext(FormContext);
     const errorPresent = errors[varName] != null;
     const requiredError = errorPresent && errors[varName].type === 'required';
-    return requiredError ? (<span className='form_error_message'>{visibleName} required!</span>) : (<span></span>);
+    const validateError = errorPresent && errors[varName].type === 'validate';
+
+    if(requiredError || validateError) {
+        const errorMessage = validateError ? validateErrorMessage :`${visibleName} required!`;
+        return (<span className='form_error_message'>{errorMessage}</span>);
+    }
+    return (<span></span>);
 };
 
-const FormTextInput = ({ varName, visibleName, type }) => {
+const FormTextInput = ({ varName, visibleName, type, validateFunction=()=> { return true; }, validateErrorMessage }) => {
     const { register } = useContext(FormContext);
-    const formParams = { required: true};
+    const formParams = { 
+        required: true,
+        validate: validateFunction
+    };
 
     return (
         <>
@@ -19,7 +28,7 @@ const FormTextInput = ({ varName, visibleName, type }) => {
                 <textarea rows='10' className="form_text_area" placeholder={visibleName} {...register(varName, formParams)} /> :
                 <input className="form_text_input"  type={type} placeholder={visibleName} {...register(varName, formParams)} />
             }
-            <FormErrorMessage varName={varName} visibleName={visibleName} />
+            <FormErrorMessage varName={varName} visibleName={visibleName} validateErrorMessage={validateErrorMessage}/>
         </>
     );
 };
--- src/components/RegisterUserForm.js
+++ src/components/RegisterUserForm.js
@@ -3,17 +3,24 @@ import { FormTextInput } from './FormControls';
 import { FormContext } from '../contexts/FormContext';
 
 const RegisterUserForm = ({ onSubmit }) => {
-    const { register, handleSubmit, formState: { errors } } = useForm();
+    const { register, handleSubmit, formState: { errors }, getValues } = useForm();
     const onSubmitClick = (data) => {
         onSubmit(data);
     };
+
+    const onValidateConfirmPass = (value) => {
+        return getValues('password') == value;
+    }
 
     return (
         <form className="input_form" onSubmit={handleSubmit(onSubmitClick)}>
             <FormContext.Provider value={{ register, errors }}>
                 <FormTextInput type='email' varName='email' visibleName='email' />
                 <FormTextInput type='password' varName='password' visibleName='password' />
-                <FormTextInput type='password' varName='confirm_password' visibleName='confirm password' />
+                <FormTextInput 
+                    type='password' varName='confirm_password' visibleName='confirm password' 
+                    validateFunction={onValidateConfirmPass} validateErrorMessage="password doesn't match"
+                />
                 <FormTextInput type='text' varName='first_name' visibleName='first name' />
                 <FormTextInput type='text' varName='last_name' visibleName='last name' />
             </FormContext.Provider>
