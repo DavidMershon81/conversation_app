ee78175964f0587dbab82a3bcbe9d49c0d643422
= <dmershon@universalhappymaker.com>
2021-07-24 17:06:46 -0700

    replaced usePostData hooks with new react-query based version

--- src/components/RegisterView.js
+++ src/components/RegisterView.js
@@ -1,39 +1,14 @@
-//import usePostData from  '../hooks/usePostData';
-import usePostDataNew from  '../hooks/usePostDataNew';
-
+import usePostData from  '../hooks/usePostData';
 import RegisterUserForm from './RegisterUserForm';
 import { LoadingBox } from './MiscControls';
-//import axios from 'axios';
-//import { useMutation } from 'react-query';
 
 
 const RegisterView = () => {
-    /*
     const onConfirm = () => {
         console.log("confirmed registration!")
     };
 
     const { post:postUser, confirmMessage, loading, error, errorMessage } = usePostData({ 
-        url:'/api/users',
-        onConfirm:onConfirm,
-        confirmText:'Registered New Account!'
-    });
-    */
-
-    /*
-    const postData = async (dataToPost) => {
-        const res = await axios.post('/api/users', dataToPost);
-        return res.data;
-    }
-    const { mutate, data, isLoading, isError, error, isSuccess } = useMutation(postData);
-    const confirmMessage = isSuccess ? 'Registered New Account!' : null;
-    */
-
-    const onConfirm = () => {
-        console.log("confirmed registration!")
-    };
-
-    const { post:postUser, confirmMessage, loading, error, errorMessage } = usePostDataNew({ 
         url:'/api/users',
         onConfirm:onConfirm,
         confirmText:'Registered New Account!'
--- src/hooks/usePostData.js
+++ src/hooks/usePostData.js
@@ -1,38 +1,23 @@
-import { useState } from 'react'
+import { useEffect } from 'react'
 import axios from 'axios';
+import { useMutation } from 'react-query';
 
 const usePostData = ({ url, onConfirm, confirmText }) => {
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(false);
-    const [errorMessage, setErrorMessage] = useState("");
-    const [confirmMessage, setConfirmMessage] = useState("");
-    const [responseData, setResponseData] = useState(null);
-
-    const onResponseError = (error) => {
-      setLoading(false);
-      setError(true);
-      setErrorMessage(error.response.data['message']);
-    };
-
-    const post = (newData) => {
-        setLoading(true);
-        setError(false);
-        setErrorMessage("");
-        setConfirmMessage("");
-        setResponseData(null);
-
-        axios.post(url, newData).then((response) => {
-            setLoading(false);
-            setResponseData(response.data);
-            setConfirmMessage(confirmText);
-
-            if(onConfirm) {
-                onConfirm();
-            }
-        }, onResponseError);
+    const postData = async (dataToPost) => {
+        const res = await axios.post(url, dataToPost);
+        return res.data;
     }
+    const { mutate, data, isLoading, isError, error, isSuccess } = useMutation(postData);
+    const confirmMessage = isSuccess ? confirmText : null;
 
-    return { post, responseData, loading, error, confirmMessage, errorMessage  };
+    useEffect(() => {
+        if(isSuccess && onConfirm) {
+            onConfirm();
+        }
+    }, [onConfirm, isSuccess])
+
+
+    return { post:mutate, responseData:data, loading:isLoading, error:isError, confirmMessage, errorMessage:error  };
 }
 
 export default usePostData;
