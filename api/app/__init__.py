241d506201906796fdf7ccf81eed9f119b7e62bb
= <dmershon@universalhappymaker.com>
2021-09-01 14:50:18 -0700

    tested blueprints with the test endpoints module

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -3,6 +3,8 @@ from dotenv import dotenv_values
 from app.database.db_model import init_db
 from app.email import init_email
 
+from app.endpoints.test import bp_test_endpoints
+
 #init flask app
 app = Flask(__name__, static_folder='../../build', static_url_path='/')
 config = dotenv_values("app_env")
@@ -13,8 +15,11 @@ app.config['SECRET_KEY'] = config['SECRET_KEY']
 #until HTTPS is set up
 app.config['SESSION_COOKIE_SAMESITE'] = 'Lax'
 
+app.register_blueprint(bp_test_endpoints)
+
 init_db(app, config)
 init_email(app)
 
+
 from app import front_end_views
-from app.endpoints import user, petition_group, member, petition, signature, login, test
+from app.endpoints import user, petition_group, member, petition, signature, login#, test
--- api/app/endpoints/test.py
+++ api/app/endpoints/test.py
@@ -1,19 +1,20 @@
-from app import app, email
+from flask import Blueprint
+from app import email
+
+bp_test_endpoints = Blueprint('test_endpoints', __name__)
     
-@app.route('/api/test_mail')
+@bp_test_endpoints.route('/api/test_mail')
 def test_mail():    
     body = """
-    Dear Recipient,
-    Don't you love testing mail? It's great to get a test mail, don't you think? 
-    I do!
-    I love mail!
-    Sincerely,
+    I'm a test Mail.
+    Some other Things.
+    Love,
     The Petition App
     """
-    mail_sent = email.send_mail(recipients=['wfwxlfkf@sharklasers.com'], subject = "A mail to test mails", body=body)
+    mail_sent = email.send_mail(recipients=['votohe9781@macauvpn.com'], subject = "A mail to test mails", body=body)
     message = 'successfully sent mail!' if mail_sent else 'failed to send mail.'
     return f"<h1>{message}</h1>"
 
-@app.route('/api/test')
+@bp_test_endpoints.route('/api/test')
 def test():    
     return f"<h1>Some Nonsense</h1>"
