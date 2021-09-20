642d1f6f31dbb00300281bd3fdab0225a0e167c1
= <dmershon@universalhappymaker.com>
2021-09-20 11:39:48 -0700

    converted endpoint for checking validations to use json instead of putting the hash in the URL

--- api/app/endpoints/validations.py
+++ api/app/endpoints/validations.py
@@ -5,14 +5,13 @@ import app.database.validation_queries as vl_queries
 
 bp_validation_endpoints = Blueprint('validation_endpoints', __name__)
 
-@bp_validation_endpoints.route('/api/validations/<validation_hash>', methods=['POST'])
-def validations(validation_hash):
-        return jsonify({'message' : f"checking validation_hash: {validation_hash}"})#, 403
+@bp_validation_endpoints.route('/api/validations', methods=['POST'])
+def validations():
+        guid =request.json['guid']
+        print(f"hit validations endpoint guid: {guid}")
+        return jsonify({'message' : f"checking validation guid: {guid}"})#, 403
 
-@bp_validation_endpoints.route('/api/test_add_validation/<user_id>', methods=['POST'])
-def test_add_validation(user_id):
-        new_validation = vl_queries.add_validation(user_id)
-        return jsonify(validation_to_dict(new_validation))
-
+"""
 def validation_to_dict(validation):
         return { 'id' : validation.id, 'guid' : validation.guid, 'user_id' : validation.user_id }
+"""
